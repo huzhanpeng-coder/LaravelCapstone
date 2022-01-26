f04e204bed2a346b0a81e173f2356b68d319d0f4
Zhanpeng Hu <hu.diego.hzp@hotmail.com>
2022-01-26 18:45:39 -0400

    first commit

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,62 @@
+@extends('common') 
+
+@section('pagetitle')
+Item List
+@endsection
+
+@section('pagename')
+Laravel Project
+@endsection
+
+@section('content')
+	
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<h1>Category List</h1>
+		</div>
+		<div class="col-md-2">
+			<a href="{{ route('categories.create') }}" class="btn btn-lg btn-block btn-primary btn-h1-spacing">Add Category</a>
+		</div>
+		<div class="col-md-12">
+			<hr />
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<table class="table">
+				<thead>
+					<th>#</th>
+					<th>Name</th>
+					<th>Created At</th>
+					<th>Last Modified</th>
+					<th></th>
+				</thead>
+				<tbody>
+					@foreach ($categories as $category)
+						@php ($i=0)
+						<tr>
+							<th>{{ $category->id }}</th>
+							<td>{{ $category->name }}</td>
+							<td style='width:100px;'>{{ date('M j, Y', strtotime($category->created_at)) }}</td>
+							<td style='width:100px;'>{{ date('M j, Y', strtotime($category->updated_at)) }}</td>
+							<td style="width: 175px;"><div style='float:left; margin-right:5px;'><a href="{{ route('categories.edit', $category->id) }}" class="btn btn-success btn-sm">Edit</a></div><div style='float:left;'>
+								@foreach ($items as $item)
+									@if($item->category_id == $category->id)
+										@php ($i=1)
+									@endif	
+								@endforeach
+								@if($i!=1)
+								{!! Form::open(['route' => ['categories.destroy', $category->id], 'method'=>'DELETE']) !!}
+									{{ Form::submit('Delete', ['class'=>'btn btn-sm btn-danger btn-block', 'style'=>'', 'onclick'=>'return confirm("Are you sure?")']) }}
+								{!! Form::close() !!}</div>	
+								@endif
+							</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div> <!-- end of .col-md-8 -->
+	</div>
+
+@endsection
